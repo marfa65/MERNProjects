924640de3e99a2431496ffbb036efb4fb2efc95e
marfa65 <marfa.gonzalez@hotmail.com>
2021-11-17 16:18:07 -0300

    tareas backend

--- server/controllers/proyectoController.js
+++ server/controllers/proyectoController.js
@@ -1,6 +1,12 @@
 const Proyecto = require("../models/Proyecto");
+const { validationResult } = require("express-validator");
 
 exports.crearProyecto = async (req, res) => {
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
   try {
     const proyecto = new Proyecto(req.body);
 
@@ -12,5 +18,77 @@ exports.crearProyecto = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send("hubo un error");
+  }
+};
+
+//obtener todos los proyectos del usuario actual
+exports.obtenerProyectos = async (req, res) => {
+  try {
+    const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({
+      creado: -1,
+    });
+    res.json({ proyectos });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("hubo un error");
+  }
+};
+
+//actualiza un proyecto
+exports.actualizarProyecto = async (req, res) => {
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  // extraer información del proyecto
+  const { nombre } = req.body;
+  const nuevoProyecto = {};
+
+  if (nombre) {
+    nuevoProyecto.nombre = nombre;
+  }
+
+  try {
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado" });
+    }
+
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+
+    proyecto = await Proyecto.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: nuevoProyecto },
+      { new: true }
+    );
+
+    res.json({ proyecto });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error en el servidor");
+  }
+};
+
+exports.eliminarProyecto = async (req, res) => {
+  try {
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado" });
+    }
+
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+
+    await Proyecto.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: "Proyecto eliminado" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error en el servidor");
   }
 };
